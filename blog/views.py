78f2de0bc25cc5d41aef34df482f2de23021b76b
spicyhoro <spicyhoro777@gmail.com>
2019-12-04 18:20:55 +0900

    잘하는중

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-def greeting(request):
+class GreetingView(View):
 
-    return HttpResponse('Hello')
+    message = 'Good Day' #클래스 변수
+
+    def get(self, *args, **kwargs):
+        return HttpResponse(self.message)
+
+greeting = GreetingView.as_view()
 
 
-# Create your views here.
+
