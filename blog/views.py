e9ecf040e4c966a2ddb89fada7af1db777d94774
spicyhoro <spicyhoro777@gmail.com>
2019-12-05 16:49:52 +0900

    함수로 재구현2

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
+def greeting_view(message):
+    def view_fn(request):
+        return HttpResponse(message)
+    return view_fn
 
-def greeting(request, message='Good Day'):
-    return HttpResponse(message)
-
-
-def morning_greeting(request):
-    return greeting(request, 'Morning to ya')
-
-
-def evening_greeting(request):
-    return greeting(request, 'Evening to ya')
+greeting = greeting_view('Good Day')
+morning_greeting = greeting_view('Morning to ya')
+evening_greeting = greeting_view('Evenign to ya')
