f6e98ffedc423143b10bbff5f8437d8909ff5661
spicyhoro <spicyhoro777@gmail.com>
2019-12-05 16:45:58 +0900

    cbv 3가지 사용법

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
-class GreetingView(View):
 
-    message = 'Good Day' #클래스 변수
-
-    def get(self, *args, **kwargs):
-        return HttpResponse(self.message)
+def greeting(request, message='Good Day'):
+    return HttpResponse(message)
 
 
-greeting = GreetingView.as_view()
+def morning_greeting(request):
+    return greeting(request, 'Morning to ya')
 
-class MorningGreetingView(GreetingView):
-    message = 'Morning to ya'
 
-morning_greeting = MorningGreetingView.as_view()
-
-evening_greeting = GreetingView.as_view(message='Evening to ya')
-
+def evening_greeting(request):
+    return greeting(request, 'Evening to ya')
